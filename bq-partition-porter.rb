579eb7b10fa5a9c7af47277b6e1fb294f9396e83
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-27 21:29:35 -0700

    bq-partition-porter version v0.1.0

---
+++ bq-partition-porter.rb
@@ -0,0 +1,15 @@
+class BqPartitionPorter < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/rdtr/bq-partition-porter/releases/download/v0.1.0/bq-partition-porter_0.1.0_darwin_amd64.zip"
+  version "0.1.0"
+  sha256 "186b87d2a47e846a3b71a53f827f7524c78310138de4fcb8b97e8985657794dd"
+
+  def install
+    bin.install "bq-partition-porter"
+  end
+
+  test do
+    
+  end
+end
